c7db8ddc4046b595246b82b8c323710c6e13bcdc
Alex Arbogast <arbogastaw@gmail.com>
2023-10-21 19:31:47 -0400

    update launch file

--- launch/za_arm_description.launch.py
+++ launch/za_arm_description.launch.py
@@ -1,40 +1,76 @@
-import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
-    load_gui = LaunchConfiguration('gui')
+    default_rviz_config = PathJoinSubstitution(
+        [FindPackageShare("za_description"), "rviz", "za_model_viewer.rviz"]
+    )
 
-    pkg_share = FindPackageShare('za_description').find('za_description')
-    za_xacro_file = os.path.join(pkg_share, 'urdf', 'za.xacro')
-    rviz_file = os.path.join(pkg_share, 'rviz', 'za_model_viewer.rviz')
-
-    robot_description = Command([FindExecutable(name='xacro'), ' ', za_xacro_file])
-
-    return LaunchDescription([
+    declared_arguments = []
+    declared_arguments.append(
         DeclareLaunchArgument(
-            'gui',
-            default_value='true'),
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{'robot_description': ParameterValue(
-                robot_description, value_type=str)}]
-        ),
-        Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            name='joint_state_publisher_gui'
-        ),
-        Node(package='rviz2',
-             executable='rviz2',
-             name='rviz2',
-             arguments=['--display-config', rviz_file])  
-    ])
+            "model",
+            default_value="za.xacro",
+            description="URDF/XACRO description file with the robot, e.g. za.xacro",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "rviz_config_file",
+            default_value=default_rviz_config,
+            description="The configuration file to use for RViz",
+        )
+    )
+
+    model = LaunchConfiguration("model")
+    rviz_config_file = LaunchConfiguration("rviz_config_file")
+
+    robot_description = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("za_description"), "urdf", model]),
+        ]
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[
+            {"robot_description": ParameterValue(robot_description, value_type=str)}
+        ],
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config_file],
+    )
+
+    nodes_to_start = [
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+        rviz_node,
+    ]
+
+    return LaunchDescription(declared_arguments + nodes_to_start)
